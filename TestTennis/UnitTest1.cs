950d19fd14fcb6d9ef2a2b33de5aa55d25094fdc
Ray Huang <eeyellow@gmail.com>
2016-04-17 07:59:56 +0800

    Init

---
+++ TestTennis/UnitTest1.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace TestTennis
+{
+    /// <summary>
+    /// 0:0 --> Love All
+    /// 1:1 --> Fifteen All
+    /// 2:2 --> Thirty All
+    ///
+    /// 3:3 --> Deuce
+    /// 4:4 --> Deuce
+    /// 5:5 --> Deuce
+    /// ========================
+    /// 1:0 --> Fifteen Love
+    /// 2:0 --> Thirty Love
+    /// 3:0 --> Forty Love
+    /// 2:1 --> Thirty Fifteen
+    /// 3:1 --> Forty Fifteen
+    /// 3:2 --> Forty Thirty
+    ///
+    /// 4:0 --> A Win
+    /// 4:1 --> A Win
+    /// 4:2 --> A Win
+    /// 5:3 --> A Win
+    /// 6:4 --> A Win
+    ///
+    /// 4:3 --> A Adventage
+    /// 5:4 --> A Adventage
+    ///
+    /// </summary>
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
