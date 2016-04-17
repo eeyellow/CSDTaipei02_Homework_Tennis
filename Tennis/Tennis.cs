e3110fa0f502e3518573c2bfaf1c981346d9f4d2
Ray Huang <eeyellow@gmail.com>
2016-04-17 08:04:11 +0800

    feat: edit func say to pass test case

--- Tennis/Tennis.cs
+++ Tennis/Tennis.cs
@@ -5,7 +5,11 @@ using System.Text;
 
 namespace VIVALACODE
 {
-    public class Tennis
+    public static class Tennis
     {
+        public static string say(int i, int j)
+        {
+            return "Love All";
+        }
     }
 }
